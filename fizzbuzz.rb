b78a10e87af5862242d82e2c0e33958bf0c1b54d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-07-07 04:44:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  if number = 3
+    print 'Fizz'
+  if ((number / 3)/(number / 3) - 1) == 0
+    print 'Fizz'
+  if ((number / 5)/(number / 5) - 1) == 0
+    print 'Buzz'
+end

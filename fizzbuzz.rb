79e99afdbb481f36364d292809d9ce6e0bf3fc96
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-07-07 05:22:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  if ((number / 3)/(number / 3) - 1) == 0 && ((number / 5)/(number / 5) - 1) == 0
-    return "FizzBuzz"
-  end
-  if ((number / 3)/(number / 3) - 1) == 0
-    return "Fizz"
-  end
-  if ((number / 5)/(number / 5) - 1) == 0
-    return "Buzz"
-  end
+def fizzbuzz(int)
+if int % 3 == 0
+  "Fizz"
+end
 end

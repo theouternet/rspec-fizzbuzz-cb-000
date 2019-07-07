401d9f84a7e13cb4f96a5d3d9a501009389f2eb9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-07-07 04:57:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,8 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-
+  if ((number / 3)/(number / 3) - 1) == 0
+  "Fizz"
+  if ((number / 5)/(number / 5) - 1) == 0
+    "Buzz"
 end
